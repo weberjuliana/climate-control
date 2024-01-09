d69a272aca2661426f48295e9730463c45f141c9
weberjuliana <juliana.weber93@gmail.com>
2024-01-09 14:51:25 -0300

    feature: insert .env-example and update mongodb conn

--- climatecontrol/src/repository/mongo_connection.py
+++ climatecontrol/src/repository/mongo_connection.py
@@ -1,1 +1,35 @@
+import logging
 
+from pymongo import MongoClient
+
+from climatecontrol.src.config.settings import settings
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class MongoDB:
+    client: MongoClient = None
+
+    @staticmethod
+    def initialize():
+        try:
+            MongoDB.client = MongoClient(settings.MONGO_URL)
+            return MongoDB.client
+        except Exception as e:
+            logger.error("Error connecting to the database", exc_info=True)
+
+    @staticmethod
+    def get_database():
+        try:
+            if MongoDB.client is None:
+                MongoDB.initialize()
+            return MongoDB.client[settings.MONGO_DB_NAME]
+        except Exception as e:
+            logger.error("Error getting the database", exc_info=True)
+
+    @staticmethod
+    def close():
+        if MongoDB.client is not None:
+            MongoDB.client.close()
+            MongoDB.client = None
